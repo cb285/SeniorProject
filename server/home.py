f15478e7a742a1fa947b8843c15a3855e73b97bf
cb285 <asapcoffee@protonmail.com>
2018-02-22 12:26:24 -0600

    set DPOT CS# to input when not used

--- server/home.py
+++ server/home.py
@@ -373,8 +373,8 @@ class Home():
                     # undo changes to allow encoder to change values
                     # set U/D# to low
                     self._zb.remote_at(dest_addr_long=bytes_mac, command=DPOT_UD_N, parameter=XB_CONF_LOW)    
-                    # set D flip flop CLR# to high (not cleared)
-                    self._zb.remote_at(dest_addr_long=bytes_mac, command=DFLIPCLR_N, parameter=XB_CONF_HIGH)
+                    # set D flip flop CLR# to input (not cleared)
+                    self._zb.remote_at(dest_addr_long=bytes_mac, command=DFLIPCLR_N, parameter=XB_CONF_DINPUT)
 
                     # update db
                     self._device_db[device_name]['level'] = level
@@ -506,7 +506,7 @@ class Home():
                self._zb.remote_at(dest_addr_long=bytes_mac, command=RELAY_STAT, parameter=XB_CONF_DINPUT)
                # set FORCE_SAMPLE_IN (D12) to input
                self._zb.remote_at(dest_addr_long=bytes_mac, command=XB_FORCE_SAMPLE_IN, parameter=XB_CONF_DINPUT)
-
+               
                # set up change detection for RELAY_CTRL (D1) and FORCE_SAMPLE_IN (D12)
                self._zb.remote_at(dest_addr_long=bytes_mac, command='IC', parameter=b'\x01002')
 
@@ -523,11 +523,8 @@ class Home():
                 # set DPOT_OUT (D2) to analog input
                 self._zb.remote_at(dest_addr_long=bytes_mac, command=DPOT_OUT, parameter=XB_CONF_ADC)
 
-                # set D flip flop CS# to low (to allow encoder to change it)
-                self._zb.remote_at(dest_addr_long=bytes_mac, command=DPOT_CS_N, parameter=XB_CONF_LOW)
-                
-                # set D flip flop CLR# to high (cleared)
-                self._zb.remote_at(dest_addr_long=bytes_mac, command=DFLIPCLR_N, parameter=XB_CONF_LOW)
+                # set D flip flop CS# to input (to allow encoder to change it)
+                self._zb.remote_at(dest_addr_long=bytes_mac, command=DFLIPCLR_N, parameter=XB_CONF_DINPUT)                
 
             # create node identifier
             node_identifier = device_type + ":" + device_mac[12:]
